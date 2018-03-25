edaabc102ae7385aea2d3d52e7ac66692c5d97ce
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-25 22:39:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
    if int % 3 == 0
      "Fizz"
-elsif int % 5 == 0
+if int % 5 == 0
     "Buzz"
-  elsif int % 4 == 0
+if int % 4 == 0
     "nil"
-  elsif int % 3 && int % 5
+if int % 3 && int % 5
   "FizzBuzz"
   end
 end
