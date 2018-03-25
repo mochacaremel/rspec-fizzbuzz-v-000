8c6280ba3629b60a17e3c14db6db901fc67f2098
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-25 21:33:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,20 @@
 require_relative './spec_helper.rb'
-
-fizzbuzz
-  fizz_3 = fizzbuzz(3)
-  puts = "Fizz"
-  fizz_5 = fizzbuzz(5)
-  puts "Buzz"
-  fizz_15 = fizzbuzz(15)
-   puts = "FizzBuzz")
-     fizz_4 = fizzbuzz(4)
-     puts = "nil"
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
 end
+
+
+
+
+# fizzbuzz
+#   fizz_3 = fizzbuzz(3)
+#   puts = "Fizz"
+#   fizz_5 = fizzbuzz(5)
+#   puts "Buzz"
+#   fizz_15 = fizzbuzz(15)
+#    puts = "FizzBuzz")
+#      fizz_4 = fizzbuzz(4)
+#      puts = "nil"
+# end
