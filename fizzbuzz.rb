336b6b5755102dc07865edbd11b3641a98968436
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-25 21:40:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
-end
+
 def fizzbuzz(int)
   if int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
   end
-end
+
 def fizzbuzz(int)
   if int % 3 && 5
     "FizzBuzz"
